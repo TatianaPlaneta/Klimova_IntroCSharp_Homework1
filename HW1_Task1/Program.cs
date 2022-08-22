3632cd9402b5fd99145a237d73ca7e9ad3866e7a
tanyaplaneta <tanyaplaneta88@gmail.com>
2022-08-22 12:11:48 +0300

    added 3 HW tasks

---
+++ HW1_Task1/Program.cs
@@ -0,0 +1,14 @@
+﻿// Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+
+// a = 5; b = 7 -> max = 7
+// a = 2 b = 10 -> max = 10
+// a = -9 b = -3 -> max = -3
+
+Console.WriteLine("Введите два целых числа a и b:");
+Console.Write("a: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("b: ");
+int b = Convert.ToInt32(Console.ReadLine());
+Console.Write("Maximum:");
+if (a > b) Console.WriteLine(a);
+else Console.WriteLine(b);
---
+++ HW1_Task2/Program.cs
@@ -0,0 +1,17 @@
+﻿// Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+
+// 2, 3, 7 -> 7
+// 44 5 78 -> 78
+// 22 3 9 -> 22
+
+Console.WriteLine("Введите три числа a, b, с: ");
+Console.Write("a: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("b: ");
+int b = Convert.ToInt32(Console.ReadLine());
+Console.Write("c: ");
+int c = Convert.ToInt32(Console.ReadLine());
+Console.Write("Maximum:");
+if (a > b && a > c) Console.WriteLine(a);
+if (b > a && b > c) Console.WriteLine(b);
+if (c > a && c > b) Console.WriteLine(c);
