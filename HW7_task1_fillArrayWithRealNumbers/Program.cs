5738902f539ac469a82ae7388128d6a458ac8371
tanyaplaneta <tanyaplaneta88@gmail.com>
2022-09-09 11:52:55 +0300

    HW7 added Tasks 1-2

---
+++ HW7_task1_fillArrayWithRealNumbers/Program.cs
@@ -0,0 +1,38 @@
+﻿// Задача 47. Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
+
+// m = 3, n = 4.
+// 0,5 7 -2 -0,2
+// 1 -3,3 8 -9,9
+// 8 7,8 -7,1 9
+
+Console.Write("Введите количество строк двумерного массива, m: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите количество столбцов  двумерного массива, n: ");
+int n = Convert.ToInt32(Console.ReadLine());
+double[,] array = new double[m, n];
+
+void FillArrayWithDouble(double[,] array)
+    {
+        for (int i = 0; i < m; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                array[i, j] = new Random().NextDouble() * 20 - 10;
+                }
+            }
+    }
+
+void PrintArray (double[,] array)
+    {
+        for (int i = 0; i < m; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                double alignNumber = Math.Round(array[i, j], 1);
+                Console.Write(alignNumber + " ");
+                }
+            Console.WriteLine();
+            }
+    }
+FillArrayWithDouble(array);
+PrintArray(array);
---
+++ HW7_task2_searchForPositionIn2DArray/Program.cs
@@ -0,0 +1,36 @@
+﻿// Задача 50. Напишите программу, которая на вход принимает позиции элемента в двумерном массиве, 
+// и возвращает значение этого элемента или же указание, что такого элемента нет. Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4        // 17 -> такого числа в массиве нет
+
+Console.Write("Введите количество строк двумерного массива, m: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите количество столбцов двумерного массива, n: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int[,] array = new int[m, n];
+ 
+for (int i = 0; i < array.GetLength(0); i++) 
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        array [i, j] = Convert.ToInt32(new Random().Next(0,10));  
+    }
+ 
+for (int i = 0; i < array.GetLength(0); i++) 
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        Console.Write(array[i,j] + "\t  ");
+        Console.WriteLine();
+    }
+
+Console.Write("Введите координату строки а: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите координату столбца b: ");
+int b = Convert.ToInt32(Console.ReadLine());
+ 
+if (a > m-1 | b > n-1) Console.WriteLine("Числа с такими координатами в данном массиве нет.");
+else
+    {
+    object c = array.GetValue(a,b);
+    Console.WriteLine("В заданном массиве это число: " + c);
+    }
