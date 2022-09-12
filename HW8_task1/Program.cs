e046090ced52ef5b118ee973570ed6a7ef90f1d6
tanyaplaneta <tanyaplaneta88@gmail.com>
2022-09-12 19:19:57 +0300

    added HW8 tasks 1 and 2

---
+++ HW8_task1/Program.cs
@@ -0,0 +1,65 @@
+﻿//Задача 54: Задайте двумерный массив. Напишите программу, которая упорядочит по убыванию элементы каждой строки двумерного массива.
+
+int m = InputNumbers("Введите количество строк массива, m: ");
+int n = InputNumbers("Введите количество столбцов массива, n: ");
+int range = InputNumbers("Введите верхнюю границу диапазона значений: от 1 до ");
+
+int InputNumbers(string input)
+{
+  Console.Write(input);
+  int output = Convert.ToInt32(Console.ReadLine());
+  return output;
+}
+
+int[,] array = new int[m, n];
+
+void CreateArray(int[,] array)
+{
+  for (int i = 0; i < array.GetLength(0); i++)
+  {
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+      array[i, j] = new Random().Next(range);
+    }
+  }
+}
+
+void WriteArray(int[,] array)
+{
+  for (int i = 0; i < array.GetLength(0); i++)
+  {
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+      Console.Write(array[i, j] + " ");
+    }
+    Console.WriteLine();
+  }
+}
+
+void OrderArrayLines(int[,] array)
+{
+  for (int i = 0; i < array.GetLength(0); i++)
+  {
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+      for (int k = 0; k < array.GetLength(1) - 1; k++)
+      {
+        if (array[i, k] < array[i, k + 1])
+        {
+          int temp = array[i, k + 1];
+          array[i, k + 1] = array[i, k];
+          array[i, k] = temp;
+        }
+      }
+    }
+  }
+}
+
+CreateArray(array);
+WriteArray(array);
+
+Console.WriteLine($"\nУпорядоченный массив: ");
+OrderArrayLines(array);
+WriteArray(array);
+
+
