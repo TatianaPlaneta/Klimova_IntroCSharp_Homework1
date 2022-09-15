a483f980bbd27953c4a2e3ac168aaa138e352694
tanyaplaneta <tanyaplaneta88@gmail.com>
2022-09-15 19:20:42 +0300

    Added HW9 Tasks 1-3

---
+++ HW9_task1_showNumbersFromMtoN/Program.cs
@@ -0,0 +1,24 @@
+﻿// Задача 64: Задайте значения M и N. Напишите программу, которая выведет все натуральные числа в промежутке от M до N.
+// M = 1; N = 5 -> "1, 2, 3, 4, 5"
+// M = 4; N = 8 -> "4, 5, 6, 7, 8"
+
+string SequenceSegmentRec(int M, int N)
+            {
+                if (N== M - 1) return "";
+                return SequenceSegmentRec(M, N-1) + "  " + Convert.ToString(N);
+            }
+try
+{
+    Console.Write("M = ");
+    int m = Convert.ToInt32(Console.ReadLine());
+    Console.Write("N = ");
+    int n = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine(SequenceSegmentRec(m, n));
+    //Console.WriteLine(SequenceRec(n));
+
+}
+catch
+{
+    Console.WriteLine("Введите натуральное число.");
+}
+
